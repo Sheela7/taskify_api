dfaf880da1ec0f5f944645a4a04ce25986b6631a
aatitkarki <aatitkarki123@gmail.com>
2022-08-02 11:29:27 +0545

    signup controller added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,8 @@
 const userModel = require('../models/user.js');
 
-
-
 module.exports.signUpUser = async (req, res) => {
-    
+    const userName = req.body.userName;
+    const email = req.body.email;
 }
 
 
