dc55970ed706ed6341d82a7039d65c5c1bb12d47
aatitkarki <aatitkarki123@gmail.com>
2022-08-03 22:45:14 +0545

    completed signup and verification feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,7 @@
 const userModel = require('../models/user.js');
 const otpService = require("../services/otp.js");
-// const passwordHash = require("../services/hashed_password.js");
+const passwordHash = require("../services/hashed_password.js");
 const emailService = require("../services/email_service.js");
-//TO DO: Take user details.
-// TO DO: Check error if exists in user input
-// TO DO: Check if email and username already exists in db
-// TO DO: set new user if not exist
-
 
 
 module.exports.signUpUser = async (req, res) => {
@@ -14,74 +9,128 @@ module.exports.signUpUser = async (req, res) => {
     const userName = req.body.name;
     const userPassword = req.body.password;
 
-    let errors = []
-
-    if(userEmail==undefined){
-        errors.push("Please enter user email")
+ const errors = validateSignupData(userName,userEmail,userPassword);   
+    if (errors.length > 0) {
+        throw errors;
     }
-    if(userName==undefined){
-        errors.push("Please enter user name")
-    }
-    if(userPassword==undefined){
-        errors.push("Please enter user password")
-    }
-    if(errors.length > 0){
-        throw errors
-    } 
-    else{
-//Queriying- (data magnu) into the user model for data with email, --userEmail.- usermodel bata userEmail ko data tanna khojya
-        const emailData = await userModel.findOne({email: userEmail});
-       //email data null xa ki xaena check grya- kina vani email data null vanya userEmail register vako xaena ra email data null xaena vni user already register xa
-        if(emailData != null){
+    else {
+        //Queriying- (data magnu) into the user model for data with email, --userEmail.- usermodel bata userEmail ko data tanna khojya
+        const emailData = await userModel.findOne({ email: userEmail });
+        //email data null xa ki xaena check grya- kina vani email data null vanya userEmail register vako xaena ra email data null xaena vni user already register xa
+        if (emailData != null) {
             throw "This email already exists";
         }
-        console.log(emailData)
-   //findone- mathi email jun mageko tesko data query grna khojeko , xa vni tyo email snga related document=line of rrow  return grxa xaena vani null return grxa.    
-        const nameData = await userModel.findOne({name: userName });
-        if(nameData != null){
+        //findone- mathi email jun mageko tesko data query grna khojeko , xa vni tyo email snga related document=line of rrow  return grxa xaena vani null return grxa.    
+        const nameData = await userModel.findOne({ name: userName });
+        if (nameData != null) {
             throw "This username already exists";
         }
-        console.log(nameData)
         //each tham ma otpService.generate() garyo vani naya otp generate hunxa so, eslai euta kunai variable maa haleraa reuse garda same otp data janxa 
-     const generatedOTP = otpService.generateOTP()
-     console.log(generatedOTP)
-    //  const generatedHashedPassword = await passwordHash.hashPassword(password);
-    //  console.log(generatedHashedPassword)
+ const userData  =  await createNewUser(userName,userEmail,userPassword);
+        
+        res.json({ "status": "success", "message": "user successfully registered", "data": userData })
+    }
+
+}
+
+const validateSignupData = (userName,userEmail,userPassword)=>{
+    let errors = []
+
+    if (userEmail == undefined || userEmail == "") {
+        errors.push("Please provide user email")
+    } else {
+
+        const isEmailValid = checkEmailValidity(userEmail);
+
+        if (!isEmailValid) {
+            errors.push("Please provide valid email")
+        }
+    }
+
+    if (userName == undefined || userName == "") {
+        errors.push("Please enter user name")
+    }
+    if (userPassword == undefined || userPassword == "") {
+        errors.push("Please enter user password");
+    }
+    return errors;
+}
+
+
+const createNewUser=async(userName,userEmail,userPassword)=>{
+const generatedOTP = otpService.generateOTP();
+        const generatedHashedPassword = await passwordHash.hashPassword(userPassword);
+        console.log(generatedHashedPassword);
         const userData = await userModel.create({
             name: userName,
-            email:  userEmail,
-            password: userPassword,
-            // password: generatedHashedPassword,
-            otp: generatedOTP 
-    });
-    console.log(userData)
- 
-    await emailService.sendOtpMail(userEmail, generatedOTP)
-    res.json({"status": "success" ,"message": "user successfully registered" , "data" : userData})
-    }
+            email: userEmail,
+            password: generatedHashedPassword,
+            otp: generatedOTP
+        });
+
+        const newUserData = {
+            '_id': userData._id,
+            'name': userData.name,
+            'email': userData.email,
+            'created': userData.created,
+            'isVerified': userData.isVerified
+        };
+        await emailService.sendOtpMail(userEmail, generatedOTP)
+        return newUserData;
+}
 
 
 
 
-module.exports.verifyEmail = (req, res) => {
+module.exports.verifyEmail = async (req, res) => {
     const OTP = req.body.otp;
-    const Email = req.body.email;
-    
+    const email = req.body.email;
 
-    let errors = []
-    if(Email==undefined){
-        errors.push("Please provide user email")
-    }
-    if(OTP==undefined){
-        errors.push("Please provide user otp")
-    }
-    if(errors.length > 0){
+   const errors = validateVerifyEmailData(email,OTP); 
+    if (errors.length > 0) {
         throw errors
-    } else{
-        const emailData = await userModel.findOne({email: Email});
-         if(emailData != null){
-            const verificationData = await userModel.t
-            OTP: otp
+    } else {
+        const emailData = await userModel.findOne({ email: email });
+
+        if (emailData == null) {
+            throw "This email is not registerd."; }
+
+        if (emailData.isVerified == true) {
+            throw "This email is already verified.";
+        }
+
+        if (emailData.otp != OTP) {
+            throw "This OTP is incorrect.";
+        } else {
+            await userModel.findByIdAndUpdate(emailData._id, {
+                isVerified: true
+            });
+            res.json({ "status": "success", "message": "User verified successfully", "data": null })
+        }
+    }
+}
+
+const validateVerifyEmailData = (email,OTP)=>{
+let errors = []
+    if (email == undefined || email == "") {
+        errors.push("Please provide email")
+    } else {
+
+        const isEmailValid = checkEmailValidity(email);
+        if (!isEmailValid) {
+            errors.push("Please provide valid email")
+        }
+    }
+    if (OTP == undefined || OTP =="") {
+        errors.push("Please provide otp")
     }
 
-}}}
+    return errors;
+}
+
+const checkEmailValidity = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const isEmailValid = re.test(String(email).toLowerCase());
+    return isEmailValid;
+}
--- services/hashed_password.js
+++ services/hashed_password.js
@@ -1,8 +1,8 @@
-// const bcrypt = require("bcrypt")
-// const saltRound = 10;
+const bcrypt = require("bcrypt")
+const saltRound = 10;
 
-// module.exports.hashPassword =async (password)=>{
-//     console.log(password)
-//     const hashedPassword= await bcrypt.hash(password,saltRound);
-//     return hashedPassword;
-// }
+module.exports.hashPassword =async (password)=>{
+    console.log(password)
+    const hashedPassword= await bcrypt.hash(password,saltRound);
+    return hashedPassword;
+}
