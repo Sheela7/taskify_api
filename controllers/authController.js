d309b1b58215fd5ef2f42ce52198eeb34f90aa0b
sheela7 <sheela7>
2022-08-09 12:41:42 +0545

    making functions and storing for previous code

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,34 +14,56 @@ module.exports.signUpUser = async (req, res) => {
     const userName = req.body.name;
     const userPassword = req.body.password;
 
+const errors = signUpValidation(userName,userEmail,userPassword);
+
+if(errors.length > 0){
+    throw errors
+} 
+else{
+    //Queriying- (data magnu) into the user model for data with email, --userEmail.- usermodel bata userEmail ko data tanna khojya
+    const emailData = await userModel.findOne({email: userEmail});
+    //email data null xa ki xaena check grya- kina vani email data null vanya userEmail register vako xaena ra email data null xaena vni user already register xa
+    //findone- mathi email jun mageko tesko data query grna khojeko , xa vni tyo email snga related document=line of rrow  return grxa xaena vani null return grxa.    
+    if(emailData != null){
+         throw "This email already exists";
+     }
+    
+     const nameData = await userModel.findOne({name: userName });
+     if(nameData != null){
+         throw "This username already exists";
+     }
+    //kai error xaena vani ani username db ma registered xaena vani naya user banawna agadi badni 
+    const userData = createUser(userName, userEmail, userPassword);
+    res.json({"status":"success", "message": "user successfully created and registered", "data": userData})
+
+}
+}
+
+const signUpValidation =(userName,userEmail,userPassword)=>{
     let errors = []
 
-    if(userEmail==undefined){
-        errors.push("Please enter user email")
+    if(userEmail==undefined || userEmail== ""){
+        errors.push("Please provide user email")
+    } else{
+        const isEmailValid = checkEmailValidity(userEmail);
+        if (!isEmailValid){
+            errors.push("Please provide valid email")
+        }
     }
-    if(userName==undefined){
+    if(userName==undefined || userName==""){
         errors.push("Please enter user name")
     }
-    if(userPassword==undefined){
+    if(userPassword==undefined || userPassword==""){
         errors.push("Please enter user password")
     }
-    if(errors.length > 0){
-        throw errors
+    return errors;
     } 
-    else{
-//Queriying- (data magnu) into the user model for data with email, --userEmail.- usermodel bata userEmail ko data tanna khojya
-        const emailData = await userModel.findOne({email: userEmail});
-       //email data null xa ki xaena check grya- kina vani email data null vanya userEmail register vako xaena ra email data null xaena vni user already register xa
-        if(emailData != null){
-            throw "This email already exists";
-        }
-        console.log(emailData)
-   //findone- mathi email jun mageko tesko data query grna khojeko , xa vni tyo email snga related document=line of rrow  return grxa xaena vani null return grxa.    
-        const nameData = await userModel.findOne({name: userName });
-        if(nameData != null){
-            throw "This username already exists";
-        }
-        console.log(nameData)
+
+    const checkEmailValidity =(email)=>{
+        
+    }
+    
+
         //each tham ma otpService.generate() garyo vani naya otp generate hunxa so, eslai euta kunai variable maa haleraa reuse garda same otp data janxa 
      const generatedOTP = otpService.generateOTP()
      console.log(generatedOTP)
