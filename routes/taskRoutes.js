770e59db7802ed4ced467be412cd42a14836eca9
sheela7 <sheela7>
2022-08-22 22:37:06 +0545

    .

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,7 +3,8 @@ const controller = require('../controllers/taskController.js');
 const errorHandler = require('../middleware/error_handler.js');
 const tokenvalidator = require('../middleware/token_validator.js');
 
-router.post('/task', errorHandler(tokenvalidator.accessTokenValidator),errorHandler(controller.taskController));
-router.get('/gettask', errorHandler(tokenvalidator.accessTokenValidator),errorHandler(controller.getTask));
 
+// router.post('/task', errorHandler(tokenvalidator.accessTokenValidator),errorHandler(controller.taskController));
+// router.get('/gettask', errorHandler(tokenvalidator.accessTokenValidator),errorHandler(controller.getTask));
+router.post('/task',  errorHandler(tokenvalidator.accessTokenValidator),errorHandler(controller.taskController),)
 module.exports = router;
